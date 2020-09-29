4ea22fddf7f4ca276b5d2827d2fca4a3013e3978
nathmotion <nmallet@excilys.com>
2020-09-29 16:23:59 +0200

    feat: add route get tasks list

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const Koa = require('koa')
-
+const Json = require('koa-json')
+const KoaRouter = require('koa-router')
 const server = new Koa()
+const router = new KoaRouter()
 
-server
-  .use((ctx) => {
-    ctx.body = 'I am your first KOA API!'
-  })
-  .listen(3001)
+server.use(Json())
+
+server.use(router.routes())
+
+router.get('/tasks', async ctx => ctx.body = 'Tasks List')
+
+server.use(async ctx =>
+    ctx.body = {'msg': 'Training Node API!'})
+
+server.listen(3001, () => console.log(' Server is started on Port 3001'))
