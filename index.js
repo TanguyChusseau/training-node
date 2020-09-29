d18e18f901616d334a764ea325b3d1578a73583f
Tanguy Chusseau <tchusseau@excilys.com>
2020-09-29 17:01:42 +0200

    feat: added tasks array

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const Koa = require('koa')
 const Json = require('koa-json')
 const KoaRouter = require('koa-router')
+
 const server = new Koa()
 const router = new KoaRouter()
+const tasks = [
+  {
+    id: 1,
+    label: 'Groceries'
+  },
+  {
+    id: 2,
+    label: 'Training Vue'
+  },
+  {
+    id: 3,
+    label: 'Training Node'
+  }
+]
 
 server.use(Json())
-
 server.use(router.routes())
 
-router.get('/tasks', async ctx => ctx.body = 'Tasks List')
+// Get route to retrieve the tasks list
+router.get('/tasks', async (ctx) => {
+  ctx.body = tasks
+})
 
-server.use(async ctx =>
-    ctx.body = {'msg': 'Training Node API!'})
+server.use(async (ctx) => {
+  ctx.body = { msg: 'Training Node API!' }
+})
 
-server.listen(3001, () => console.log(' Server is started on Port 3001'))
+server.listen(3001, () => console.log('Server is started on Port 3001'))
