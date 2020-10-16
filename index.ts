58ced51652a2bf32d8694de786b0d33d492cd67c
Tanguy Chusseau <tchusseau@excilys.com>
2020-10-16 11:38:24 +0200

    WIP: PR fixes

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import * as Router from 'koa-router'
 import * as bodyParser from 'koa-bodyparser'
 import * as cors from '@koa/cors'
 import * as json from 'koa-json'
+
 import { Task } from './model/task'
 
 const app = new Koa()
@@ -30,18 +31,20 @@ app.use(json())
 app.use(router.routes())
 
 // GET route to retrieve the tasks list
-router.get('/tasks', (request: any) => {
+router.get('/tasks', (request) => {
   request.body = tasks
 })
 
 // POST route to add a task
-router.post('/tasks', (request: any) => {
-  tasks.push(request.request.body)
+router.post('/tasks', (request) => {
+  const newTask: Task = request.request.body
+  newTask.id = Math.floor(Math.random() * 200) + 1
+  tasks.push(newTask)
   request.body = tasks
 })
 
 // DELETE route to delete a task with a matching id
-router.delete('/tasks/:id', (request: any) => {
+router.delete('/tasks/:id', (request) => {
   tasks = tasks.filter(({ id }) => id !== parseInt(request.params.id, 10))
   request.body = tasks
 })
